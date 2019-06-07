86d62aeb9bf89b64a7eb2988ce36493ce6574c25
Tolu Adepoju <tolu@cu-vpn-dabfp-172.21.34.5.int.colorado.edu>
2019-06-06 23:48:48 -0700

    Minimum routines to get data from server

--- connect.py
+++ connect.py
@@ -18,12 +18,41 @@ server.auth.sign_in(tableau_auth)
 print("\nSuccessfully authenticated {} to {} ".format(username, server.baseurl))
 
 #all sites
-all_sites, pagination_item = server.sites.get()
+allSites, pagination_item = server.sites.get()
+print("\nThere are {} sites on the server ".format(pagination_item.total_available))
 
-# print all the site names and ids
-# print("id", "name", "content_url", "state")
-for site in all_sites:
+for site in allSites:
     print(site.id, site.name, site.content_url, site.state)
+
+#all users
+allUsers, pagination_item = server.users.get()
+print("\nThere are {} users on site ".format(pagination_item.total_available))
+print("UserId, UserName, UserEmail, LastLogin")
+for user in allUsers:
+    print(user.id, user.name, user.email, user.last_login)
+
+
+#all projects
+allProjects, pagination_item = server.projects.get()
+print("\nThere are {} projects on site ".format(pagination_item.total_available))
+for project in allProjects:
+    print(project._id, project.name, project.description, project.content_permissions, project.parent_id)
+
+#all schedules
+allSchedules, pagination_item = server.schedules.get()
+print("\nThere are {} schedules on site ".format(pagination_item.total_available))
+for schedule in allSchedules:
+    print(schedule.id, schedule.name, schedule._created_at, schedule.schedule_type, schedule._state, schedule._next_run_at, schedule._end_schedule_at)
+
+allDataSources, pagination_item = server.datasources.get()
+print("\nThere are {} datasources on site ".format(pagination_item.total_available))
+for dataSource in allDataSources:
+    print(dataSource._id, dataSource.name, dataSource._content_url, dataSource._created_at, dataSource._updated_at, dataSource._project_id, dataSource._project_name, dataSource._datasource_type)
+
+allViews, pagination_item = server.views.get()
+print("\nThere are {} views on site ".format(pagination_item.total_available))
+for view in allViews:
+    print(view._id, view._name, view._content_url, view._total_views, view._image, view._preview_image, view._workbook_id, view._csv)
 
 # with server.auth.sign_in(tableau_auth):
 #     all_datasources, pagination_item = server.datasources.get()
